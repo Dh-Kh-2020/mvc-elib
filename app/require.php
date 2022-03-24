64d80c79bfa992a3d505cc97e131aa6a9e9c96b1
Dhoha Alkhorasani <raihanahit2016@gmail.com>
2022-03-24 18:20:14 +0300

    fix require.php

--- app/require.php
+++ app/require.php
@@ -1,12 +1,7 @@
 <?php
-    //Require libraries from folder libraries
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
+    require_once 'config/DBConn.php';
 
-    require_once 'helpers/session_helper.php';
-
-    require_once 'config/config.php';
+    require_once 'controllers/auth_session.php';
 
     //Instantiate core class
-    $init = new Core();
+    // $init = new Core();
